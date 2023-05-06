7bb899c8e732ac28f9a32fba85a38d62080613bc
Derek Nissen <dereknissen23@gmail.com>
2023-05-06 15:28:25 -0500

    a lot got done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # dataScienceProject
 
 https://www.youtube.com/watch?v=c4Af2FcgamA - For components of the project
+https://devinrourke.github.io/posts/python-milesplit-part1/ - Milesplit scraping
--- pullData.py
+++ pullData.py
@@ -1,9 +1,55 @@
+from bs4 import BeautifulSoup
 import requests
+import json
 
-url = "http://api.milesplit.com/test/echo"
+url = "https://mo.milesplit.com/"
 
-def pullData():
-    response = requests.get(url)
-    print(response)
+seasons = ["/cross-country", "/outdoor-track-and-field"]
+event = ["/5000m?year=", "/100m?year=", "/200m?year=", "/400m?year=",
+         "/800m?year=", "/1600m?year=", "/3200m?year=", "/100mH?year=",
+         "/110mH?year=", "/300mH?year=", "/HJ?year=", "/LJ?year=", "/TJ?year=",
+         "/PV?year=", "/SP?year=", "/DT?year="]
 
-pullData()
+page = ['1','2','3','4','5','6','7','8','9','10',
+        '11','12','13','14','15','16','17','18','19','20']
+
+# Raypec ID is 10686
+# Rockhurst ID is 10692
+def addRosterToTeams(teamID):
+    scrapeURL = url + "teams/" + str(teamID) + "/roster"
+    page = requests.get(scrapeURL)
+    soup = BeautifulSoup(page.text, "html.parser")
+    athletes = soup.find_all("div", attrs={"class":"data-point w-30 w-md-50 d-flex align-items-center"})
+    teamName = soup.find_all("h1")[0].text
+
+    print("------------------ BREAK -----------------")
+
+
+    for i, athlete in enumerate(athletes):
+        athleteLink = str(athlete.a).split("\"")[1]
+        athleteName = (athlete.text).strip() # Strip new line characters
+        athleteWords = athleteName.split(", ")
+        # Associate link
+        athletes[i] = [athleteName, athleteLink]
+
+    
+    # Update json file
+    teamDict = {teamName: athletes}
+    oldJsonFile = open("teams.JSON", "r") # Opens file for editing
+    oldJsonDict = json.load(oldJsonFile) # Gets a copy of old dictionary
+    jsonFile = open("teams.JSON", "w")
+
+    #print(type(oldJsonDict))
+    #print(oldJsonDict)
+    #print(type(teamDict))
+    #print(teamDict)
+
+    teamDict.update(oldJsonDict)
+
+    json.dump(teamDict, jsonFile, indent = 6,)
+
+
+    
+
+
+addRosterToTeams(10686)
